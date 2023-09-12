4e0bbc5f9afb47ca8f983d4e3e1a04d0c7c8241a
Lirys <iamyourmarkieboo@gmail.com>
2023-09-12 16:49:22 +0800

    Added fancy scrolling animation

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,11 +1,27 @@
 let listItem = document.querySelectorAll(".nav-section");
+let selection = document.querySelectorAll(".section");
 
-for (const elem of listItem) {
-    elem.addEventListener("click", ()=>{
+for (let i = 0; i < listItem.length; i++) {
+    listItem[i].addEventListener("click", ()=>{
         let activeListItem = document.querySelector(".nav-section.active");
-        if(elem !== activeListItem){
+        if(listItem[i] !== activeListItem){
             activeListItem.className = "nav-section";
-            elem.className = "nav-section active";
+            listItem[i].className = "nav-section active";
         }
+        location.href = "#"+selection[i].getAttribute("id");
     })
 }
+
+window.addEventListener("scroll", () => {
+    for (let i = 0; i < listItem.length; i++) {
+        let scrolled = window.scrollY;
+        let elemTop =  selection[i].getBoundingClientRect().top;
+        let elemHeight = selection[i].offsetHeight;
+        if(elemTop <= scrolled && scrolled < elemTop + elemHeight){
+            for (const elem of listItem) {
+                elem.className = "nav-section";
+            }
+            listItem[i].className = "nav-section active";
+        }
+    }
+})
--- index.php
+++ index.php
@@ -7,19 +7,56 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/CSS/navigation.css">
     <link rel="stylesheet" href="/CSS/no-margin.css">
+    <link rel="stylesheet" href="/CSS/index.css">
     <title>PHP-Arrays</title>
 </head>
 <body>
 
-<div class="nav">
-    <div class="inner-container">
-        <ul id="list">
-            <li class="nav-section active">Home</li>
-            <li class="nav-section ">Sample 1</li>
-            <li class="nav-section ">Sample 2</li>
-        </ul>
+    <div class="nav">
+        <div class="nav-inner">
+            <ul id="list">
+                <li class="nav-section active">Home</li>
+                <li class="nav-section">Sample 1</li>
+                <li class="nav-section">Sample 2</li>
+            </ul>
+        </div>
     </div>
-</div>
+
+    <section class="section" id="home">
+        <div class="container">
+            <div class="inner-container">
+                <article class="article">
+                    <h1 class="article-title">PHP Function Examples</h1>
+                    <h4 class="sub-heading">Learn the different kinds of array implementations!</h4>
+                    <p></p>
+                </article>
+            </div>
+        </div>
+    </section>
+
+    <section class="section" id="arrays-one">
+        <div class="container">
+            <div class="inner-container">
+                <article class="article article-right">
+                    <h1 class="article-title">Function Example 1</h1>
+                    <h4 class="sub-heading"></h4>
+                    <p></p>
+                </article>
+            </div>
+        </div>
+    </section>
+
+    <section class="section" id="arrays-two">
+        <div class="container">
+            <div class="inner-container">
+                <article class="article article-left">
+                    <h1 class="article-title">Function Example 2</h1>
+                    <h4 class="sub-heading"></h4>
+                    <p></p>
+                </article>
+            </div>
+        </div>
+    </section>
 
 <script src="./JS/navigation.js"></script>
 
